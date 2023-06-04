ea8ef8801c56724470e98da368e244eef85e23f7
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-06-04 20:30:54 +0530

    added lyrical search

--- main.js
+++ main.js
@@ -3,27 +3,25 @@ console.log("Welcome to Spotify")
 //Initialize the variables
 let songIndex = 0;
 let audioElement = new Audio('general/songs/1.mp3');
-audioElement.src='general/songs/1.mp3';
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 let masterSongName = document.getElementById('masterSongName');
 let title = document.getElementById('title');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
-let songItemPlays = Array.from(document.getElementsByClassName('songItemPlay'));
 
 let songs = [
-    {id: '0', songName: "Your Eyes", filePath: "general/songs/1.mp3", coverPath: "general/covers/1.jpg"},
-    {id: '1',songName: "Play Date", filePath: "general/songs/2.mp3", coverPath: "general/covers/2.jpeg"},
-    {id: '2',songName: "SaiRat Zaale ji", filePath: "general/songs/3.mp3", coverPath: "general/covers/3.jpg"},
-    {id: '3',songName: "Shikhayat", filePath: "general/songs/4.mp3", coverPath: "general/covers/4.jpg"},
-    {id: '4',songName: "Love Story", filePath: "general/songs/5.mp3", coverPath: "general/covers/5.jpg"},
-    {id: '5',songName: "Day Dreamin", filePath: "general/songs/6.mp3", coverPath: "general/covers/6.jpg"},
-    {id: '6',songName: "Tera Chehra", filePath: "general/songs/7.mp3", coverPath: "general/covers/7.jpeg"},
-    {id: '7',songName: "Kolu Kolu", filePath: "general/songs/8.mp3", coverPath: "general/covers/8.jpeg"},
-    {id: '8',songName: "Jab Saiyaan", filePath: "general/songs/9.mp3", coverPath: "general/covers/9.jpeg"},
-    {id: '9',songName: "Naa Madhi", filePath: "general/songs/10.mp3", coverPath: "general/covers/10.jpg"},
-    {id: '10',songName: "Mastaru Mastaru", filePath: "general/songs/11.mp3", coverPath: "general/covers/11.jpg"}
+    {id: '0', songName: "Your Eyes", filePath: "general/songs/1.mp3", coverPath: "general/covers/1.jpg", lyrics: "Your eyes got my heart falling for you You're messing with my heart and I still wanna love you I wanna spend my life drowning in your eyes It's my only prayer"},
+    {id: '1', songName: "Play Date", filePath: "general/songs/2.mp3", coverPath: "general/covers/2.jpeg", lyrics: "You call me on the telephone, you feel so far away You tell me to come over, there's some games you want to play I'm walking to your house, nobody's home Just me and you and you and me alone"},
+    {id: '2', songName: "SaiRat Zaale ji", filePath: "general/songs/3.mp3", coverPath: "general/covers/3.jpg", lyrics: "alguj vaaj nabhaat bhaltach zhalay aaj algad aali manaat pahilach tarani hi laaj"},
+    {id: '3', songName: "Shikhayat", filePath: "general/songs/4.mp3", coverPath: "general/covers/4.jpg", lyrics: "Kisi Ki Yaad Mein Shaamein Guzaarne Ke Liye Kaleja Chahiye Khud Ko Maarne Ke Liye"},
+    {id: '4', songName: "Love Story", filePath: "general/songs/5.mp3", coverPath: "general/covers/5.jpg", lyrics: "We were both young when I first saw you I close my eyes and the flashback starts I'm standin' there On a balcony in summer air See the lights, see the party, the ball gowns See you make your way through the crowd And say, Hello Little did I know"},
+    {id: '5', songName: "Day Dreamin", filePath: "general/songs/6.mp3", coverPath: "general/covers/6.jpg", lyrics: "You walked in Caught my attention I've never seen A man with so much dimension"},
+    {id: '6', songName: "Tera Chehra", filePath: "general/songs/7.mp3", coverPath: "general/covers/7.jpeg", lyrics: "Meri bechainiyo ko chain mill jaye Tera chehra jab nazar aaye Tera chehra jab nazar aaye Mere diwanepan ko sabra mill jaaye Tera chehra jab nazar aaye"},
+    {id: '7', songName: "Kolu Kolu", filePath: "general/songs/8.mp3", coverPath: "general/covers/8.jpeg", lyrics: "Kolu koloo koloyamma Kommaa chivarana poolu poose kolo Puvvulaanti sinnadhemo Moggayindhi sigguthoti koloyamma"},
+    {id: '8', songName: "Jab Saiyaan", filePath: "general/songs/9.mp3", coverPath: "general/covers/9.jpeg", lyrics: "Jab Saiyan Aaye Sham Ko Toh Lag Gaye Chand Mere Naam Ko Jab Saiyan Aaye Sham Ko Toh Lag Gaye Chand Mere Naam Ko"},
+    {id: '9', songName: "Naa Madhi", filePath: "general/songs/10.mp3", coverPath: "general/covers/10.jpg", lyrics: "Naa Madhi Puvvadhi Vaadipothu Unnadhi Chinnadi Chey Vidi Chitrahimse Ayinadhi Ninnu Thalachuku Mathi Chediponu Devuda Ani Digulai Ten To Five Ponu Paiki Badhaga Kanabadaneee Manasu Pagilina Manishinile"},
+    {id: '10', songName: "Mastaru Mastaru", filePath: "general/songs/11.mp3", coverPath: "general/covers/11.jpg", lyrics: "Seetakalam Manasu Nee Manasuna Chotadiginde Sitakumalle Neetho Adugese Maatadiginde Neeku Nuvve Gundelone Annadantha Vinnaale Anthakanna Mundugaane Yendhuko Avunannale Inkapaina Neeku Naaku Prema Pathale"}
 ]
 
 let songs_list = document.getElementsByClassName('songItemContainer')[0];
@@ -37,8 +35,8 @@ songs.forEach(element => {
         <span class="songlistplay"><span class="timestamp"><i id="${id}" class="fa-regular songItemPlay fa-circle-play fa-2x"></i></span></span>
     `;
     songs_list.appendChild(song);
-    
 });
+
 songItems.forEach((element,i)=>{
     element.getElementsByTagName("img")[0].src = songs[i].coverPath;
     element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
@@ -48,13 +46,14 @@ songItems.forEach((element,i)=>{
 
 let search_result = document.getElementsByClassName('search_result')[0];
 songs.forEach(element => {
-    const { id, songName, filePath, coverPath } = element;
+    const { id, songName, filePath, coverPath, lyrics } = element;
     let card = document.createElement('a');
     card.classList.add('card');
     card.href = '#' + id;
     card.innerHTML = `
     <img src="${coverPath}" alt="">
         <div class="content">${songName}</div>
+        <div class="lyrics">${lyrics}</div>
         <i id="${id}" class="fa-regular songItemPlay fa-circle-play fa-2x"></i>
     `;
     search_result.appendChild(card);
@@ -66,9 +65,11 @@ input.addEventListener('keyup', ()=>{
     let items = search_result.getElementsByTagName('a');
     for (let i = 0; i < items.length; i++) {
         let as = items[i].getElementsByClassName('content')[0];
+        let as1 = items[i].getElementsByClassName('lyrics')[0];
         let text_value = as.textContent || as.innerText;
+        let text_value1 = as1.textContent || as1.innerText;
 
-        if (text_value.toUpperCase().indexOf(input_value) > -1) {
+        if (text_value.toUpperCase().indexOf(input_value) > -1 || text_value1.toUpperCase().indexOf(input_value) > -1) {
             items[i].style.display = "flex";
         } else {
             items[i].style.display = "none";
@@ -88,6 +89,7 @@ const makeAllPlays = ()=>{
         element.classList.add('fa-circle-play');
     })
 }
+
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         index = parseInt(e.target.id);
@@ -139,8 +141,6 @@ masterPlay.addEventListener('click', ()=>{
         gif.style.opacity=1;
         title.innerText = songs[songIndex].songName;
         masterSongName.innerText = songs[songIndex].songName;
-        /*songItemPlays[songIndex].classList.remove('fa-circle-play');
-        songItemPlays[songIndex].classList.add('fa-circle-pause');*/
     }
     else{
         makeAllPlays();
@@ -152,6 +152,7 @@ masterPlay.addEventListener('click', ()=>{
         masterSongName.innerText = songs[songIndex].songName;
     }
 })
+
 // Listen to Events
 audioElement.addEventListener('timeupdate', ()=>{
     let curr = audioElement.currentTime;
