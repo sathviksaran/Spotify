75059a069dd0eb5e2f82c0e3a10c4805ad409f2d
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-06-05 00:18:55 +0530

    modified bottom

--- main.js
+++ main.js
@@ -254,3 +254,8 @@ closeIcon.addEventListener("click", () => {
         search_item[i].style.display="none";
     }
 })
+
+let volicon = document.getElementsByClassName('vol-icon');
+volicon.addEventListener("mouseover", ()=>{
+    document.getElementById('volumebar').style.display="flex";
+})
